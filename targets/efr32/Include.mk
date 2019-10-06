b34c72ddd6565e6c82254b94a2ec247bbb6e0ad5
Štefan Šimek <simek@triaxis.sk>
2019-10-06 19:52:17 +0200

    efr32: add RAIL chip name

---
+++ targets/efr32/Include.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (c) %Y triaxis s.r.o.
+# Licensed under the MIT license. See LICENSE.txt file in the repository root
+# for full license information.
+#
+# efr32/Include.mk
+#
+
+EFM32_RAIL_CHIP = efr32xg1x
