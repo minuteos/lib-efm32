73114c64877b469fc7da53527e5098650981ac3d
Štefan Šimek <simek@triaxis.sk>
2019-10-22 21:11:25 +0200

    Add efr32-series1 target

---
+++ targets/efr32-series1/Include.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 triaxis s.r.o.
+# Licensed under the MIT license. See LICENSE.txt file in the repository root
+# for full license information.
+#
+# efr32-series1/Include.mk
+#
+# Makefile additions for all SiLabs EFR32 Series 1 MCUs
+#
+
+TARGETS += efr32 efm32-series1 cortex-m4f
--- targets/efr32bg13p/Include.mk
+++ targets/efr32bg13p/Include.mk
@@ -6,7 +6,7 @@
 # efr32bg13p/Include.mk
 #
 
-TARGETS += efr32 efm32-series1 cortex-m4f
+TARGETS += efr32-series1
 
 EFM32_DEVICE ?= EFM32BG13P
 # use the most full-featured part by default
