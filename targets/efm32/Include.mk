f7b9eeee99d989cb1f0d66eb8a792806312eb828
Štefan Šimek <simek@triaxis.sk>
2019-11-21 14:19:50 +0100

    efm32: make mbedtls and bootloader libs available for build

--- targets/efm32/Include.mk
+++ targets/efm32/Include.mk
@@ -6,6 +6,11 @@
 # efm32/Include.mk - Makefile additions for SiLabs EFM32 MCUs
 #
 
+EFM32_MAKEFILE := $(call curmake)
+EFM32_LIB_DIR = $(call parentdir,$(call parentdir,$(call parentdir,$(EFM32_MAKEFILE))))
+
+EFM32_TOOL_DIR = $(EFM32_LIB_DIR)tools/
+
 EFM32_DEV_ROOT ?= /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/
 EFM32_SDKS ?= $(EFM32_DEV_ROOT)sdks/gecko_sdk_suite/
 SI_COMMANDER ?= $(EFM32_DEV_ROOT)adapter_packs/commander/Commander.app/Contents/MacOS/commander
@@ -25,21 +30,25 @@ EFM32_SDK_ORIGIN_DEVICE = $(EFM32_SDK_ORIGIN)platform/Device/SiliconLabs/$(EFM32
 EFM32_SDK_ORIGIN_CMSIS = $(EFM32_SDK_ORIGIN)platform/CMSIS/
 EFM32_SDK_ORIGIN_EMLIB = $(EFM32_SDK_ORIGIN)platform/emlib/
 EFM32_SDK_ORIGIN_BOOT = $(EFM32_SDK_ORIGIN)platform/bootloader/
+EFM32_SDK_ORIGIN_MBEDTLS = $(EFM32_SDK_ORIGIN)util/third_party/mbedtls/
 
 EFM32_SDK_DEVICE = $(OBJDIR)efm32-device/
 EFM32_SDK_CMSIS = $(OBJDIR)efm32-cmsis/
 EFM32_SDK_EMLIB = $(OBJDIR)efm32-emlib/
 EFM32_SDK_BOOT = $(OBJDIR)efm32-boot/
+EFM32_SDK_MBEDTLS = $(OBJDIR)efm32-mbedtls/
 
 EFM32_DEVICE_INCLUDE = $(EFM32_SDK_DEVICE)Include/
 EFM32_CMSIS_INCLUDE = $(EFM32_SDK_CMSIS)Include/
 EFM32_EMLIB_INCLUDE = $(EFM32_SDK_EMLIB)inc/
 EFM32_BOOT_INCLUDE = $(EFM32_SDK_BOOT) $(EFM32_SDK_BOOT)api/
+EFM32_MBEDTLS_INCLUDE = $(EFM32_SDK_MBEDTLS)include/
 EFM32_PART_HEADER := $(firstword $(sort $(filter-out Simplicity,$(notdir $(wildcard $(EFM32_SDK_ORIGIN)platform/Device/SiliconLabs/$(EFM32_DEVICE)/Include/$(EFM32_PART))))))
 EFM32_SYSTEM_SOURCE := $(firstword $(sort $(filter-out Simplicity,$(notdir $(wildcard $(EFM32_SDK_ORIGIN)platform/Device/SiliconLabs/$(EFM32_DEVICE)/Source/*.c)))))
 EFM32_SYSTEM_SOURCE_PATH := $(EFM32_SDK_DEVICE)Source/$(EFM32_SYSTEM_SOURCE)
 
-INCLUDE_DIRS += $(EFM32_DEVICE_INCLUDE) $(EFM32_CMSIS_INCLUDE) $(EFM32_EMLIB_INCLUDE) $(EFM32_BOOT_INCLUDE)
+INCLUDE_DIRS += $(EFM32_DEVICE_INCLUDE) $(EFM32_CMSIS_INCLUDE) $(EFM32_EMLIB_INCLUDE) $(EFM32_BOOT_INCLUDE) $(EFM32_MBEDTLS_INCLUDE)
+LIB_DIRS += $(EFM32_SDK_BOOT)build/lib/
 ADDITIONAL_SOURCES += $(EFM32_SYSTEM_SOURCE_PATH)
 DEFINES += EFM32_DEVICE=$(EFM32_DEVICE) EFM32_PART_HEADER=$(EFM32_PART_HEADER)
 COMPONENTS += hw emlib
@@ -64,7 +73,7 @@ prebuild: efm32_sdk
 
 $(EFM32_SYSTEM_SOURCE_PATH): efm32_sdk
 
-efm32_sdk: $(EFM32_SDK_DEVICE) $(EFM32_SDK_CMSIS) $(EFM32_SDK_EMLIB) $(EFM32_SDK_BOOT)
+efm32_sdk: $(EFM32_SDK_DEVICE) $(EFM32_SDK_CMSIS) $(EFM32_SDK_EMLIB) $(EFM32_SDK_BOOT) $(EFM32_SDK_MBEDTLS)
 	$(info Using EFM32 SDK in $(EFM32_SDK_ORIGIN), device $(EFM32_DEVICE), part header $(EFM32_PART_HEADER))
 
 $(EFM32_SDK_DEVICE)Source/: $(EFM32_SDK_DEVICE:/=)
@@ -80,3 +89,6 @@ $(EFM32_SDK_EMLIB): $(OBJDIR)
 
 $(EFM32_SDK_BOOT): $(OBJDIR)
 	@$(LN) -snf $(EFM32_SDK_ORIGIN_BOOT) $(EFM32_SDK_BOOT:/=)
+
+$(EFM32_SDK_MBEDTLS) : $(OBJDIR)
+	@$(LN) -snf $(EFM32_SDK_ORIGIN_MBEDTLS) $(EFM32_SDK_MBEDTLS:/=)
