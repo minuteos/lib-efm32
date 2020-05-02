9455494776ceea9e455a4fa7d8ed766f43784355
Štefan Šimek <simek@triaxis.sk>
2020-05-02 16:58:06 +0200

    efm32: DEVINFO - add unique ID extraction

--- targets/efm32/hw/DEVINFO.h
+++ targets/efm32/hw/DEVINFO.h
@@ -10,13 +10,22 @@
 
 #include <base/base.h>
 
+#include <base/Span.h>
+
 #undef DEVINFO
 #define DEVINFO    CM_PERIPHERAL(_DEVINFO, DEVINFO_BASE)
 
 class _DEVINFO : public DEVINFO_TypeDef
 {
 public:
+    //! Gets the 48-bit Extended Unique Identifier of the device
+    Span GetEUI48() const { return Span((const void*)&EUI48L, 6); }
+    //! Gets the 64-bit Unique Identifier of the device
+    Span GetUniqueID() const { return Span((const void*)&UNIQUEL, 8); }
+
+    //! Gets the size of FLASH in bytes
     uint32_t FlashSize() const { return ((this->MSIZE & _DEVINFO_MSIZE_FLASH_MASK) >> _DEVINFO_MSIZE_FLASH_SHIFT) << 10; }
+    //! Gets the size of RAM in bytes
     uint32_t RamSize() const { return ((this->MSIZE & _DEVINFO_MSIZE_SRAM_MASK) >> _DEVINFO_MSIZE_SRAM_SHIFT) << 10; }
 };
 
