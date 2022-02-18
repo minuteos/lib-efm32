8488f813b8f58eb110ad9b7cfa8f8ead2fb82567
Štefan Šimek <simek@triaxis.sk>
2022-02-18 15:30:46 +0100

    efm32: kernel - add wake reason statistics support

---
+++ targets/efm32/kernel/platform.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * efm32/kernel/platform.h
+ */
+
+#ifndef PLATFORM_WAKE_REASON_COUNT
+#define PLATFORM_WAKE_REASON_COUNT  EXT_IRQ_COUNT
+#endif
+
+#include_next <kernel/platform.h>
